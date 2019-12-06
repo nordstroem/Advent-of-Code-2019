e3d0e98822956c01a0f991b68e02a27be53c5028
Johan Nordström <cjs.nordstrom@gmail.com>
2019-12-06 17:09:00 +0100

    Cleanup day 4 a bit.

--- day4.py
+++ day4.py
@@ -1,21 +1,16 @@
 from aocutil import *
+import itertools
 
 total_number = 0
 
 for num in range(273025, 767253 + 1):
     digits = [int(c) for c in str(num)]
+
     never_decreasing = digits == sorted(digits)
-    
-    groups = [[digits[0]]]
-    for d in digits[1:]:
-        if d == groups[-1][0]:
-            groups[-1].append(d)
-        else:
-            groups.append([d])
-    
+    groups = [list(g) for k, g in itertools.groupby(digits)]
     two_adjacent = any(len(dgs) == 2 for dgs in groups)
+    
     if never_decreasing and two_adjacent:
         total_number = total_number + 1
-
 
 print(total_number)
