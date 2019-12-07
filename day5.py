8a0b38205a49cf8f2516e2dd23f5a964a802fcff
Johan Nordström <cjs.nordstrom@gmail.com>
2019-12-07 12:52:38 +0100

    Working day5 part1.

--- day5.py
+++ day5.py
@@ -1,22 +1,46 @@
 from aocutil import *
-from operator import *
 import itertools
+
+memory = [int(x) for x in read_file("input5.txt").split(',')]
+mode_or_zero = lambda i: modes[-(i + 1)] if i < len(modes) else 0
+
+def add(src1, src2, dst, modes):
+    a = memory[src1] if mode_or_zero(0) == 0 else src1
+    b = memory[src2] if mode_or_zero(1) == 0 else src2
+    memory[dst] = a + b
+
+def mul(src1, src2, dst, modes):
+    a = memory[src1] if mode_or_zero(0) == 0 else src1
+    b = memory[src2] if mode_or_zero(1) == 0 else src2
+    memory[dst] = a * b
+
+def get_input(src, _=None):
+    memory[src] = int(input("Enter input value: "))
+
+def write_output(src, modes):
+    value = memory[src] if mode_or_zero(0) == 0 else src
+    print(value)
 
 operations = {
     1: add,
-    2: mul
+    2: mul,
+    3: get_input,
+    4: write_output
 }
 
-memory = [int(x) for x in read_file("input2.txt").split(',')]
+parameter_counts = {
+    1: 3,
+    2: 3,
+    3: 1,
+    4: 1
+}
 
 pc = 0
 while memory[pc] != 99:
-    opcode = memory[pc]
-    src1 = memory[pc + 1]
-    src2 = memory[pc + 2]
-    dst = memory[pc + 3] 
-    memory[dst] = operations[opcode](memory[src1], memory[src2])
-    pc = pc + 4
-
-
-print(memory[0])
+    instruction = str(memory[pc])
+    opcode = int(instruction[-2:])
+    modes = [int(c) for c in instruction[:-2]] 
+    parameters = [memory[pc + i] for i in range(1, parameter_counts[opcode] + 1)]
+    operations[opcode](*parameters, modes)
+    pc = pc + parameter_counts[opcode] + 1
+    
