b2a843d02fc052ab1de7cbe1eafc21f0cc74551a
Johan Nordström <cjs.nordstrom@gmail.com>
2019-12-08 18:25:50 +0100

    Cleanup.

--- day8.py
+++ day8.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 data = [int(c) for c in read_file("input8.txt").strip()]
 
 img = np.array(data).reshape(-1, 6, 25)
-layer = min([(np.count_nonzero(img[i,:] == 0), i) for i in range(img.shape[0])])[1]
+layer = np.argmin(np.count_nonzero(img == 0, axis=(1, 2)))
 print(np.count_nonzero(img[layer,:] == 1) * np.count_nonzero(img[layer,:] == 2))
 
 final_image = np.full((6, 25), -1)
-for i in range(img.shape[0]):
-    indicies_to_update = np.logical_and(img[i,:] != 2, final_image == -1)
-    final_image[indicies_to_update] = img[i, indicies_to_update]
+for sub_image in img:
+    indicies_to_update = np.logical_and(sub_image != 2, final_image == -1)
+    final_image[indicies_to_update] = sub_image[indicies_to_update]
 
-plt.imshow(final_image)
+plt.imshow(final_image, cmap='gray')
 plt.show()
