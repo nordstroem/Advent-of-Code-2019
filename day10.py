297a3d702f921c5e8e2a6240ab8c67765ba09dae
Johan Nordström <cjs.nordstrom@gmail.com>
2019-12-14 14:55:30 +0100

    Minor cleanup.

--- day10.py
+++ day10.py
@@ -10,22 +10,14 @@ def visible_count(tar_index):
     for ind in np.argwhere(asteroid_map):
         if np.array_equal(ind, tar_index):
             continue
-        
         delta = tar_index - ind
-        k = gcd(delta[0], delta[1])
+        k = gcd(*delta)
         minimum_delta = delta // k
-        visible = True
-        for i in range(1, k):
-            ind2 = ind + i * minimum_delta
-            if asteroid_map[ind2[0], ind2[1]]:
-                visible = False
-                break
-        if visible:
+        test_indices = [ind + i * minimum_delta for i in range(1, k)]
+        if not any(asteroid_map[x, y] for (x, y) in test_indices):
             count += 1
     return count
 
-best = 0
-for ind in np.argwhere(asteroid_map):
-    best = max(best, visible_count(ind))
+best = max(visible_count(ind) for ind in np.argwhere(asteroid_map))
 
 print(best)
