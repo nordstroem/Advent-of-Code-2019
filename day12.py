c889611d5ced7bebe9e47bd3b7fc808124d4394e
Johan Nordström <cjs.nordstrom@gmail.com>
2019-12-15 14:07:51 +0100

    Cleanup day12 part1.

--- day12.py
+++ day12.py
@@ -2,6 +2,8 @@ from aocutil import read_lines
 from functools import reduce
 from itertools import combinations
 import re
+import numpy as np
+
 
 class Planet:
     def __init__(self, position, velocity):
@@ -12,33 +14,17 @@ class Planet:
         return "pos=<x={}, y={}, z={}> vel=<x={}, y={}, z={}>".format(*self.position, *self.velocity)
 
     def energy(self):
-        pot = reduce(lambda a, b: a + abs(b), self.position, 0)
-        kin = reduce(lambda a, b: a + abs(b), self.velocity, 0)
-        return pot * kin
+        return np.sum(np.abs(self.position)) * np.sum(np.abs(self.velocity))
 
-    def step(self):
-        self.position = [p + v for (p, v) in zip(self.position, self.velocity)]
-
-def sign(a):
-    if a > 0:
-        return 1
-    if a < 0:
-        return -1
-    return 0
-
-planets = []
-for line in read_lines("input12.txt"):
-    position = list(map(int, re.findall(r'-*\d+', line)))
-    planets.append(Planet(position, [0, 0, 0]))
+planets = [Planet(np.fromiter(re.findall(r'-*\d+', line), int), np.zeros(3, int)) for line in read_lines("input12.txt")]
 
 for i in range(1000):
     for p1, p2 in combinations(planets, 2):
-        gravity = [sign(v1 - v2) for (v1, v2) in zip(p1.position, p2.position)]
-        for i in range(3):
-            p2.velocity[i] += gravity[i]
-            p1.velocity[i] -= gravity[i]
+        gravity = np.sign(p2.position - p1.position)
+        p1.velocity += gravity
+        p2.velocity -= gravity
 
     for p in planets:
-        p.step()
+        p.position += p.velocity
 
 print(reduce(lambda a, b: a + b.energy(), planets, 0))
