547fd6fb30e0466416a87130afd4e3ace6bb5e72
Johan Nordström <cjs.nordstrom@gmail.com>
2019-12-17 19:52:58 +0100

    Working part2 on day12.

--- day12.py
+++ day12.py
@@ -3,6 +3,7 @@ from functools import reduce
 from itertools import combinations
 import re
 import numpy as np
+import math
 
 
 class Planet:
@@ -18,7 +19,21 @@ class Planet:
 
 planets = [Planet(np.fromiter(re.findall(r'-*\d+', line), int), np.zeros(3, int)) for line in read_lines("input12.txt")]
 
-for i in range(1000):
+get_hash = lambda i: tuple(reduce(lambda a, b: a + [b.position[i], b.velocity[i]], planets, []))
+seen = [{}, {}, {}]
+result = [0, 0, 0]
+for i in range(1000000000):
+
+    for j in range(3):
+        hsh = get_hash(j)
+        if hsh in seen[j] and not result[j]:
+            result[j] = i
+        else:
+            seen[j][hsh] = i
+
+    if all(result):
+        break
+
     for p1, p2 in combinations(planets, 2):
         gravity = np.sign(p2.position - p1.position)
         p1.velocity += gravity
@@ -27,4 +42,9 @@ for i in range(1000):
     for p in planets:
         p.position += p.velocity
 
-print(reduce(lambda a, b: a + b.energy(), planets, 0))
+gcd1 = math.gcd(result[0], result[1])
+val1 = result[0] * result[1] // gcd1
+gcd2 = math.gcd(val1, result[2])
+print(val1 * result[2] // gcd2)
+
+#print(reduce(lambda a, b: a + b.energy(), planets, 0))
