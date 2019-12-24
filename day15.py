769890c62043460a10a46abf4d21aac8c8281101
Johan Nordström <cjs.nordstrom@gmail.com>
2019-12-24 19:41:19 +0100

    Working day 15 part2. (not clean)

--- day15.py
+++ day15.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import time
 import random
 import time
+import json
+import copy
 
 memory = [int(x) for x in read_file("input15.txt").split(',')]
 
@@ -42,6 +44,8 @@ def goto_unvisited():
             next_key, next_node = min(unvisited_neighbors, key=lambda nb: distances[nb])
         else:
             # Otherwise, backtrack our taken path and search for an unvisited neighbor.
+            if not key_path:
+                return None
             reverse_key = reverse_keys[key_path.pop()]
             input_queue.put(reverse_key)
             new_status = output_queue.get()
@@ -55,7 +59,8 @@ def goto_unvisited():
     key_path.append(next_key)
     return next_node
 
-while last_status != 2:
+grid = {"(0, 0)": 0}
+while True:
     # Find the neighbors of this node
     neighbors = []
     itms = list(deltas.items())
@@ -63,14 +68,20 @@ while last_status != 2:
     for key, delta in itms:
         input_queue.put(key)
         last_status = output_queue.get()
-        if last_status == 2:
-            print(distances[current_node] + 1)
-            assert False, "Found destination node!"
+        ng_node = (current_node[0] + delta[0], current_node[1] + delta[1])
+        if last_status == 0:
+            grid[str(ng_node)] = 1
         elif last_status == 1:
-            neighbors.append((key, (current_node[0] + delta[0], current_node[1] + delta[1])))
+            grid[str(ng_node)] = 0
+        else:
+            print(distances[current_node] + 1)
+            grid[str(ng_node)] = 2
+
+        if last_status != 0:
+            neighbors.append((key, ng_node))
             input_queue.put(reverse_keys[key])
             le = output_queue.get()
-    
+        
     all_neighbors[current_node] = neighbors
 
     for key, n in neighbors:
@@ -82,5 +93,40 @@ while last_status != 2:
     visited[current_node] = True
 
     current_node = goto_unvisited()
-    print(current_node)
-    assert current_node not in visited
+    if not current_node:
+        break
+
+
+'''with open('day14_map.json', 'w') as fp:
+    json.dump(grid, fp)
+
+
+grid = {}
+with open('day14_map.json', 'r') as fp:
+    grid = json.load(fp)
+'''
+
+### Part 2
+def str_to_int_tuple(key):
+    a, b = tuple(key.replace('(','').replace(')','').replace(' ', '').split(','))
+    return int(a), int(b)
+
+grid = {str_to_int_tuple(key): item for key, item in grid.items()}
+has_updated = True
+iterations = 0
+deltas = {1: (0, 1), 2: (0, -1), 3: (-1, 0), 4: (1, 0)}
+while has_updated:
+    updated_grid = copy.copy(grid)
+    has_updated = False
+    for node, key in grid.items():
+        if key == 2:
+            for delta in deltas.values():
+                ng_node = (node[0] + delta[0], node[1] + delta[1])
+                if grid[ng_node] == 0:
+                    updated_grid[ng_node] = 2
+                    has_updated = True
+    
+    grid = updated_grid
+    iterations += 1
+
+print(iterations - 1)
